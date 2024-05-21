14e9b213643c9e79ad163de7e0c7a9a1f2099cd4
zahra kazemi <zahra.k199002@gmail.com>
2024-05-21 14:42:53 +0330

    add record screen

--- app/src/main/java/com/androidproject/voicenotemanager/ui/TopBar.kt
+++ app/src/main/java/com/androidproject/voicenotemanager/ui/TopBar.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Mic
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,6 +19,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -65,21 +68,35 @@ fun NoteListTopBar(
 }
 
 @Composable
-fun RecordTopBar(time: String) {
+fun RecordTopBar(time: String, noteName: String) {
     TopAppBar(
         title = {
+            Text(
+                modifier = Modifier.padding(start = 5.dp),
+                text = noteName,
+                style = MaterialTheme.typography.titleLarge
+            )
             Row(
                 Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 10.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Icon(Icons.Rounded.Menu, "")
+                Icon(
+                    modifier = Modifier.size(30.dp),
+                    imageVector = Icons.Rounded.Mic,
+                    contentDescription = ""
+                )
                 Text(
                     text = time,
                     modifier = Modifier.padding(start = 5.dp),
-                    style = MaterialTheme.typography.titleLarge
                 )
+            }
+        },
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(Icons.Filled.ArrowBack, "")
             }
         },
         modifier = Modifier.fillMaxWidth(),
@@ -114,7 +131,7 @@ private fun PreviewApp() {
 @Preview
 @Composable
 private fun PreviewAppRecord() {
-    RecordTopBar("1:23")
+    RecordTopBar("1:23", "math")
 }
 
 @Preview
