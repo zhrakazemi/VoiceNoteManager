cd61d4b7bc0083e3dbce9a5b7107ecb6303239f6
zahra kazemi <zahra.k199002@gmail.com>
2024-05-23 02:17:44 +0330

    completing queries

--- app/src/main/java/com/androidproject/voicenotemanager/data/DAO.kt
+++ app/src/main/java/com/androidproject/voicenotemanager/data/DAO.kt
@@ -2,6 +2,7 @@ package com.androidproject.voicenotemanager.data
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Upsert
 
 @Dao
 interface DAO {
@@ -9,8 +10,22 @@ interface DAO {
     @Query("SELECT * FROM note , attachment WHERE id =:Noteid")
     fun getNote(Noteid: String): LocalNote
 
-    @Query("SELECT * FROM note WHERE categoryId =id")
+    @Query("SELECT * FROM note WHERE categoryId =:id")
     fun getCategory(id: String): LocalNote
 
+    @Query("SELECT * FROM category  ")
+    fun getCategoryList(): LocalNote
 
+
+    @Query("SELECT * FROM attachment WHERE id =:id ")
+    fun getAttachment(id: String): LocalNote
+
+    @Upsert
+    suspend fun upsertNote(note: LocalNote)
+
+    @Upsert
+    suspend fun upsertCategory(category: LocalCategory)
+
+    @Upsert
+    suspend fun upsertattachment(attachment: LocalAttachment)
 }
