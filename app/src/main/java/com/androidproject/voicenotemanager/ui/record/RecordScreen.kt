8e2df73d3ba124a366dc4fdaf7d352b314205b2c
Milad <milad.tavakoli81@gmail.com>
2024-05-28 23:47:17 +0330

    add record and save functions

--- app/src/main/java/com/androidproject/voicenotemanager/ui/record/RecordScreen.kt
+++ app/src/main/java/com/androidproject/voicenotemanager/ui/record/RecordScreen.kt
@@ -3,6 +3,8 @@ package com.androidproject.voicenotemanager.ui.record
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.FloatingActionButton
@@ -13,6 +15,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.androidproject.voicenotemanager.NavigationActions
 import com.androidproject.voicenotemanager.ui.RecordTopBar
@@ -20,12 +27,11 @@ import com.androidproject.voicenotemanager.ui.RecordTopBar
 @Composable
 fun RecordScreen(
     noteId: String?,
-    noteName: String = "",
-    time: String = "1:12",
     onBack: () -> Unit,
     navigationActions: NavigationActions,
     recordViewModel: RecordViewModel = hiltViewModel()
-    ) {
+) {
+    val recordUiState by recordViewModel.uiState.collectAsState()
     Scaffold(modifier = Modifier.fillMaxWidth(), floatingActionButton = {
         FloatingActionButton(onClick = {
             navigationActions.navigateToNote(noteId)
@@ -33,11 +39,23 @@ fun RecordScreen(
             Icon(Icons.Filled.Edit, "")
         }
     }, topBar = {
-        RecordTopBar(time, noteName, onBack)
+        RecordTopBar(
+            recordUiState.timer, recordUiState.noteName, onBack, recordViewModel
+        )
     }) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            val recordUiState by recordViewModel.uiState.collectAsState()
-            Text(text = recordUiState.recordedText)
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                modifier = Modifier.padding(10.dp),
+                textAlign = TextAlign.Start,
+                text = recordUiState.recordedText,
+                style = TextStyle(textDirection = TextDirection.Rtl),
+                fontSize = 20.sp
+            )
         }
     }
 }
--- app/src/main/java/com/androidproject/voicenotemanager/ui/record/RecordViewModel.kt
+++ app/src/main/java/com/androidproject/voicenotemanager/ui/record/RecordViewModel.kt
@@ -1,12 +1,16 @@
 package com.androidproject.voicenotemanager.ui.record
 
+import android.content.Context
+import android.content.Intent
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.androidproject.voicenotemanager.DestinationsArgs
+import com.androidproject.voicenotemanager.ForegroundService
+import com.androidproject.voicenotemanager.VOSKApi
 import com.androidproject.voicenotemanager.data.Repository
-import com.androidproject.voicenotemanager.voskApi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,24 +21,40 @@ import javax.inject.Inject
 
 
 data class RecordUiState(
+    val noteName: String = "",
     val recordedText: String = "",
-    val time: String = ""
-) {
-}
+    val timer: Long = 0L,
+)
+
+var otimer: Long = 0
+var orunning: Boolean = true
+
 
 @HiltViewModel
 class RecordViewModel @Inject constructor(
+    @ApplicationContext private val appContext: Context,
+    val voskApi: VOSKApi,
     private val repository: Repository,
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(RecordUiState())
     val uiState: StateFlow<RecordUiState> = _uiState.asStateFlow()
     private val noteId: String = savedStateHandle[DestinationsArgs.NOTE_ID_ARG]!!
 
-
     init {
         getNote()
-
+        viewModelScope.launch {
+            while (true) {
+                delay(1000)
+                if (!voskApi.getPause) {
+                    _uiState.update { currentState ->
+                        currentState.copy(timer = _uiState.value.timer + 1)
+                    }
+                    otimer = _uiState.value.timer
+                    orunning = !voskApi.getPause
+                }
+            }
+        }
     }
 
     private fun getNote() {
@@ -42,23 +62,68 @@ class RecordViewModel @Inject constructor(
             _uiState.update { currentState ->
                 currentState.copy(recordedText = repository.getNote(noteId).recordedVoice)
             }
+            _uiState.update { currentState ->
+                currentState.copy(noteName = repository.getNote(noteId).name)
+            }
         }
     }
 
+    private var firstStart = true
+
     private fun startRecording() {
+        if (firstStart) {
+            firstStart = false
+            voskApi.recognizeMicrophone()
+        }
         viewModelScope.launch {
-            delay(1000)
-            while (voskApi.getPause) {
+            Intent(appContext, ForegroundService::class.java).also {
+                it.action = ForegroundService.Actions.START.toString()
+                appContext.startService(it)
+            }
+            delay(500)
+            while (!voskApi.getPause) {
                 delay(1000)
+                viewModelScope.launch {
+                    var usernote = repository.getNote(noteId)
+                    usernote = usernote.copy(recordedVoice = voskApi.mainText)
+                    repository.updateNote(
+                        noteId, usernote.name, usernote.categoryId,
+                        usernote.recordedVoice, usernote.userNotes
+                    )
+                }
                 _uiState.update { currentState ->
                     currentState.copy(recordedText = voskApi.mainText)
                 }
             }
         }
     }
-//        Intent(Context, ForegroundService::class.java).also {
-//            it.action = ForegroundService.Actions.START.toString()
-//            startService(it)
-//        }
-//    })
+
+    fun stopRecording() {
+        Intent(appContext, ForegroundService::class.java).also {
+            it.action = ForegroundService.Actions.STOP.toString()
+            appContext.startService(it)
+        }
+    }
+
+    fun startStopRecording() {
+        if (voskApi.model == null)
+            return
+        if (voskApi.getPause) {
+            startRecording()
+        } else {
+            stopRecording()
+        }
+    }
+
+    fun share(){
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, _uiState.value.recordedText)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        appContext.startActivity(shareIntent)
+    }
 }
